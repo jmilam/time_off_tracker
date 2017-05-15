44651f72ccb63ace5f0e91820787a628c90fce01
Jason Milam <jasonlmilam@gmail.com>
2017-05-15 15:45:41 -0400

    Add payroll users to email notifications

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -9,7 +9,7 @@ FactoryGirl.define do
     admin						true
     vacation_total 	100
     personal_total	100
-    payroll					false
+    payroll					true
     account_manager	true
     site						"Endura Products"
   end
